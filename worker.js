cbc167fb69eed29e80503fda14e1903d875e222c
仲冬梅 <z.dmnj@163.com>
2019-04-19 13:53:20 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,10 @@
+onmessage = function(event) {
+  let data = event.data
+  console.log('worker', data)
+  let ans = fibonacci(data)
+  postMessage(ans)
+}
 
+function fibonacci (n) {
+  return n < 2 ? n: fibonacci(n-1) + fibonacci(n-2)
+}
